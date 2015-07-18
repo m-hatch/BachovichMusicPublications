ab27b7ac369912649c1703d48d0a0c40f9e42926
m-hatch <montehatch@gmail.com>
2015-07-18 16:23:17 -0400

    archive files

---
+++ assets/js/sticky.js
@@ -0,0 +1,33 @@
+/* Sticky Top Nav and Sidebar on Scroll */
+
+( function( $ ) {
+  $( document ).ready(function() {
+
+    // insure content window height
+    var windowHeight = $(window).height();
+    $('.page-content-wrapper').css("min-height", windowHeight - 72);
+
+    // update on window resize
+    $( window ).resize(function() {
+      windowHeight = $(window).height();
+      $('.page-content-wrapper').css("min-height", windowHeight - 72);
+    });
+
+    // stick/unstick on scroll
+    $(window).on('scroll',function() {
+      var scrolltop = $(this).scrollTop();
+      var pos = $('#wrapper').position().top - 52;
+      
+      if(scrolltop >= pos) {
+        $('#sidebar-wrapper').removeClass( "sidebar-unsticky" ).addClass( "sidebar-sticky" );
+        $('#top-nav').removeClass( "nav-unsticky" ).addClass( "nav-sticky" );
+      }
+       
+      else {
+        $('#sidebar-wrapper').removeClass( "sidebar-sticky" ).addClass( "sidebar-unsticky" );
+        $('#top-nav').removeClass( "nav-sticky" ).addClass( "nav-unsticky" );
+      }
+    });
+
+  });
+} )( jQuery );
