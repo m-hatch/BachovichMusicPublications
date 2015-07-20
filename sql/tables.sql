28b2efbc72c08131e5fa4225d399531b9ddca241
m-hatch <montehatch@gmail.com>
2015-07-20 14:08:53 -0400

    add sheetmusic imgs

--- sql/tables.sql
+++ sql/tables.sql
@@ -29,10 +29,29 @@ CREATE TABLE Medias(
   type VARCHAR(10),
   title VARCHAR(100) NOT NULL,
   description TEXT,
-  price DECIMAL(8,2),
+  price DECIMAL(8,2) NOT NULL,
   img VARCHAR(100),
   shipping DECIMAL(6,2),
   PRIMARY KEY ( media_id ),
+  FOREIGN KEY (artist_id) 
+    REFERENCES Artists(artist_id)
+);
+
+CREATE TABLE SheetMusic(
+  music_id VARCHAR(4) NOT NULL,
+  artist_id INT NOT NULL,
+  composer VARCHAR(100),
+  type VARCHAR(40),
+  sub_type1 VARCHAR(40),
+  sub_type2 VARCHAR(40),
+  title VARCHAR(100) NOT NULL,
+  duration VARCHAR(15),
+  contents VARCHAR(100),
+  description TEXT,
+  price DECIMAL(8,2) NOT NULL,
+  img VARCHAR(100),
+  shipping DECIMAL(6,2),
+  PRIMARY KEY ( music_id ),
   FOREIGN KEY (artist_id) 
     REFERENCES Artists(artist_id)
 );
