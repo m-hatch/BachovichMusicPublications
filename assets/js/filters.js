c3315809a5f26c79de82ec466a950ac3f5f7fecb
m-hatch <montehatch@gmail.com>
2015-07-23 15:09:47 -0400

    create search_artists filter

---
+++ assets/js/filters.js
@@ -0,0 +1,34 @@
+/* --------------------------------------------------------------- *
+*                           search artists                         *
+* ---------------------------------------------------------------- */
+app.filter('search_artists', function($log) {
+  return function(input, query) {
+    // return all artists if nothing in query box
+    if (!query) return input;
+
+    //split query terms by space character
+    var terms = query.split(' ');
+    var output = [];
+
+    // iterate through input array
+    input.forEach(function(artist){
+      var found = false;
+      passTest = true;
+
+      // iterate through terms found in query box
+      terms.forEach(function(term){
+       
+        // if all terms are found set boolean to true
+        found = (artist.fname.toLowerCase().indexOf(term.toLowerCase()) > -1) 
+          || (artist.lname.toLowerCase().indexOf(term.toLowerCase()) > -1);
+        
+        passTest = passTest && found;
+      });
+
+      // Add artist to output array only if passTest is true -- all search terms were found in artist
+      if (passTest) { output.push(artist); }
+    });
+
+    return output;
+  }
+})
