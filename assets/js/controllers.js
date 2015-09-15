6ff8fa1112713dc2eb295885b1895e1eb39e1ce7
m-hatch <montehatch@gmail.com>
2015-09-15 19:07:40 -0400

    move search filter to controller

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -70,9 +70,40 @@ app.controller('searchCtrl', function($scope, $route, $location, SearchService){
 // search results
 app.controller('searchResultCtrl', function($scope, $http, SearchService){
   $scope.search_music = SearchService.get();
+  $scope.no_results = false;
   $http.get('api/search')
   .success(function(response) {
-    $scope.musics = response;
+    filtered_result = function(input, query) {
+      // return all artists if nothing in query box
+      if (!query) return input;
+
+      //split query terms by space character
+      var terms = query.split(' ');
+      var output = [];
+
+      // iterate through input array
+      input.forEach(function(artist){
+        var found = false;
+        passTest = true;
+
+        // iterate through terms found in query box
+        terms.forEach(function(term){
+          // if all terms are found set boolean to true
+          found = (artist.fname.toLowerCase().indexOf(term.toLowerCase()) > -1) 
+            || (artist.lname.toLowerCase().indexOf(term.toLowerCase()) > -1);
+          passTest = passTest && found;
+        });
+
+        // Add artist to output array only if passTest is true -- all search terms were found in artist
+        if (passTest) { output.push(artist); }
+      });
+      return output;
+    }
+
+    $scope.musics = filtered_result(response, $scope.search_music);
+    if (!$scope.musics.length) {
+      $scope.no_results = true;
+    }
   });
 });
 
