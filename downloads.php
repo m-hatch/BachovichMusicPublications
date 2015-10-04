08cec1b938b2a2a9b3336812704cc21f2973f93e
m-hatch <montehatch@gmail.com>
2015-10-04 14:27:52 -0400

    add downloads.php

---
+++ downloads.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" CONTENT="Welcome to Bachovich Music Publications! Extraordinary percussion literature for the 21st century!">
+  <meta name="keywords" CONTENT="Bachovich, Downloads, Supplimental, Content">
+  <title>Bachovich Music Publications - Downloads</title>
+  <link href="assets/css/reset.css" rel="stylesheet" type="text/css" />
+  <link href="assets/css/general.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+  <div id="maincontent">
+
+    <h1>Downloads</h1>
+    
+    <ul>
+      <li class="downloads"><strong>Techniques and Basic Patterns for Congas, Timbales, and Bongos</strong><br />
+      by Rolando Morales Matos<br />
+      <p>These two downloads contain all the audio mp3 files associated with <em>Techniques and Basic Patterns for Congas, Timbales, and Bongos</em>. The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/TABP-for-CTB-disc1.zip" class="downloadButton" download>Download disc 1 (73.4MB)</a>
+      <a href="downloads/TABP-for-CTB-disc2.zip" class="downloadButton" download>Download disc 2 (80.3MB)</a>
+      </li>
+      <li class="downloads"><strong>Six Pieces for Pedal Piano, op 56 &amp; 58</strong><br />
+      by Robert Schumann/arr. Randy Max<br />
+      <p>This download contains all 6 recordings of <em>Six Pieces for Pedal Piano, op 56 &amp; 58</em> performed by Randy Max (timpani) with Shelley Katz (piano). The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/Randy-Max-CD.zip" class="downloadButton" download>Download (147.7MB)</a>
+      </li>
+      <li class="downloads"><strong>Sakpatá for percussion ensemble and tape</strong><br />
+      by Javier Diaz<br />
+      <p>This download contains the audio mp3 of the tape accompaniment. Clicking on this will load the mp3 into the audio player built into your browser. To save a copy of the mp3 file to your computer, right click (or go to "file" in your browser menu) and select "Save page". This will save a copy of the mp3 file to whichever folder you specify.</p>
+      <a href="downloads/Sakpata-tape.mp3" class="downloadButton" download>Download (9.1MB)</a>
+      </li>
+      <li class="downloads"><strong>Facing the Mirror for percussion quartet and tape</strong><br />
+       by Joseph Pereira<br />
+       <p>There are two download files available. One file has a click and the other is without the click. Clicking on this will load the mp3 into the audio player built into your browser. To save a copy of the mp3 file to your computer, right click (or go to "file" in your browser menu) and select "Save page". This will save a copy of the mp3 file to whichever folder you specify.</p>
+      <a href="downloads/Facing-The-Mirror-click.mp3" class="downloadButton" download>Download with click (17MB)</a>
+      <a href="downloads/Facing-The-Mirror-no-click.mp3" class="downloadButton" download>Download without click (17MB)</a></li>
+      <li class="downloads"><strong>In Other Words for solo percussion and tape</strong><br />
+       by Joseph Pereira<br />
+       <p>This download contains the audio mp3 of the accompaniment. Clicking on this will load the mp3 into the audio player built into your browser. To save a copy of the mp3 file to your computer, right click (or go to "file" in your browser menu) and select "Save page". This will save a copy of the mp3 file to whichever folder you specify.</p>
+      <a href="downloads/In-Other-Words.mp3" class="downloadButton" download>Download (13MB)</a>
+      </li>
+      <li class="downloads"><strong>Integrated Elements No. 3 'Divide by Five' for gyil and fixed media</strong><br />
+       by Robert McClure<br />
+       <p>This download contains the audio mp3 of the accompaniment.Clicking on this will load the mp3 into the audio player built into your browser. To save a copy of the mp3 file to your computer, right click (or go to "file" in your browser menu) and select "Save page". This will save a copy of the mp3 file to whichever folder you specify.</p>
+      <a href="downloads/IE3-Fixed-Media.mp3" class="downloadButton" download>Download (9MB)</a>
+      </li>
+
+      <li class="downloads"><strong>Tajin for solo percussion and tape</strong><br />
+       by James Campbell<br />
+       <p>This download contains the audio mp3 of the accompaniment. Clicking on this will load the mp3 into the audio player built into your browser. To save a copy of the mp3 file to your computer, right click (or go to "file" in your browser menu) and select "Save page". This will save a copy of the mp3 file to whichever folder you specify.</p>
+      <a href="downloads/TAJIN-SoundscapeOutput1-2.mp3" class="downloadButton" download>Download (8MB)</a>
+      </li>
+      <li class="downloads"><strong>Nine French-American Rudimental Solos Volume 1</strong><br />
+       by Joe Tompkins<br />
+       <p>This download contains the audio recordings of all the movements performed by Joe Tompkins. The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/Volume-1.zip" class="downloadButton" download>Download (13MB)</a>
+      </li>
+      <li class="downloads"><strong>Nine French-American Rudimental Solos Volume 2</strong><br />
+       by Joe Tompkins<br />
+       <p>This download contains the audio recordings of all the movements performed by Joe Tompkins. The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/Volume-2.zip" class="downloadButton" download>Download (17MB)</a>
+      </li>
+      <li class="downloads"><strong>MbiraTab - Vol 1</strong><br />
+      by Michael B. Williams<br />
+      <p>These two downloads contain all the audio mp3 files associated with <em>MbiraTab - Vol 1</em>. The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/Mbira-Vol-1-mp3s.zip" class="downloadButton" download>Download (24.4MB)</a>
+      </li>
+      <li class="downloads"><strong>MbiraTab - Vol 2</strong><br />
+      by Michael B. Williams<br />
+      <p>These two downloads contain all the audio mp3 files associated with <em>MbiraTab - Vol 2</em>. The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/Mbira-Vol-2-mp3s.zip" class="downloadButton" download>Download (25.8MB)</a>
+      </li>
+      <li class="downloads"><strong>MbiraTab - Vol 3</strong><br />
+      by Michael B. Williams<br />
+      <p>These two downloads contain all the audio mp3 files associated with <em>MbiraTab - Vol 3</em>. The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/Mbira-Vol-3-mp3s.zip" class="downloadButton" download>Download (32.9MB)</a>
+      </li>
+      <li class="downloads"><strong>MbiraTab - Vol 4</strong><br />
+      by Michael B. Williams<br />
+      <p>These two downloads contain all the audio mp3 files associated with <em>MbiraTab - Vol 4</em>. The download is in .zip format. Files can be expanded by double clicking on .zip file once downloaded.</p>
+      <a href="downloads/Mbira-Vol-4-mp3s.zip" class="downloadButton" download>Download (17.2MB)</a>
+      </li>
+      <li class="downloads"><strong>Music For Botany</strong><br />
+      by Jeremy Muller<br />
+      <p>Accompaniment audio for <em>Music For Botany</em></p>
+      <a href="downloads/MusicForBotanyAccomp.mp3" class="downloadButton" download>Download (9.7MB)</a>
+      </li>
+    </ul>
+
+  </div>
+
+</body>
+</html>
